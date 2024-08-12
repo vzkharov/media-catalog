d323971c748af51b233b315f561e4a070a271b7c
vadzim.l <vadzim.l@realmond.com>
2024-08-12 23:21:05 +0300

    update: `use-pagination`

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,5 +1,37 @@
+'use client'
+
+import { useMemo, useCallback } from 'react'
+
 /** @todo from `media-pagination` */
 
-const usePagination = () => {}
+type UsePaginationUtilsArgs = {
+	page: number
+	onPageChange: (page: number) => unknown
 
-export { usePagination }
+	total?: number
+	displayPages?: number
+}
+
+const usePaginationUtils = ({ page, total, onPageChange, displayPages = 3 }: UsePaginationUtilsArgs) => {
+	const pages = useMemo(() => {
+		const range = new Array(displayPages + 1).fill(0).map((_, idx) => idx + page - 2)
+
+		if (page > 0) {
+			range.shift()
+		} else {
+			range.pop()
+		}
+
+		return range
+	}, [page, displayPages])
+
+	const next = useCallback(() => total && page < total && onPageChange(page + 1), [onPageChange, total, page])
+	const previos = useCallback(() => page > 1 && onPageChange(page - 1), [onPageChange, page])
+
+	return { next, previos, pages, isPrevios: page > 1 }
+}
+
+type UsePaginationUtilsReturn = ReturnType<typeof usePaginationUtils>
+
+export { usePaginationUtils }
+export type { UsePaginationUtilsArgs, UsePaginationUtilsReturn }
